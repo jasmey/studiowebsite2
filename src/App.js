c578e5dd63217096d76cfcea936f595d74e70147
Jasmine Meyer <110313151+jasmey@users.noreply.github.com>
2024-07-10 07:50:16 -0400

    buttons are reactive, stuff working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar.js';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Services from './components/pages/Services';
-// import Products from './components/pages/Products';
-// import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -13,10 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact component={Home} />
-          {/* <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} /> */}
+          <Route path='/' element={<Home />} />
         </Routes>
       </Router>
     </>
